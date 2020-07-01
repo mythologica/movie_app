7c869f0f09403f5844fd269ae21f380fa1d8e362
mythologica <pgmmind@gmail.com>
2020-07-01 20:16:56 +0900

    array map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const movieList = [
+  { key: 1, movieName: "로마의 휴일" , moviePhoto:"https://lh3.googleusercontent.com/proxy/BuHfFafONtNQkOYZz_-m67ry7Esos8VbQ9b_99oHlm8jNjdvxXv1YeisqEQbgDnfwDDOe_u-B0RncquGCo_oXxUhBKwO3tp_5nU4AH_tGfJBzc0oBmtezVVYAl4JiZ83IRnXwBRmsw"},
+  { key: 2, movieName: "메트릭스" , moviePhoto:"https://img9.yna.co.kr/photo/cms/2019/08/03/08/PCM20190803000008005_P2.jpg"},
+  { key: 3, movieName: "기생충" , moviePhoto:"https://img9.yna.co.kr/photo/cms/2019/08/03/08/PCM20190803000008005_P2.jpg"},
+  { key: 4, movieName: "나비효과" , moviePhoto:"https://img9.yna.co.kr/photo/cms/2019/08/03/08/PCM20190803000008005_P2.jpg"},
+];
+
+function Movie() {
+  return <h2>Movie</h2>;
+}
+
+//function MovieItem(props) {
+function MovieItem({ movieName, moviePhoto }) {
+  return (
+    <>
+      <h3>{movieName}</h3>
+      <img src={moviePhoto} />
+    </>
+  );
+}
 
 function App() {
+  // return <div className="App"></div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Hello React~!</div>
+      <Movie />
+      {movieList.map((movieItem) => (
+        <MovieItem key={movieItem.key} movieName={movieItem.movieName} moviePhoto={movieItem.moviePhoto} />
+      ))}
+    </>
   );
 }
 
---
+++ src/components/Worker.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Worker() {
+  return (
+    <>
+      <h2>Worker</h2>
+    </>
+  );
+}
+
+export default Worker;
